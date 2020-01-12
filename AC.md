9ef1864176fe39cbdf9e2491acf99945a38c70dc
jasmine.zhu <jasmine.zhu@oocl.com>
2020-01-12 15:47:08 +0800

    test: should return II when roman calculator add given I and I

---
+++ AC.md
@@ -0,0 +1,6 @@
+## 1
+```
+given: Roman number I and I
+when:  Roman numbers add
+then:  equal II 
+```
---
+++ test/RomanCalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanCalculatorTest {
+
+  @Test
+  public void should_return_II_when_roman_number_add_given_I_and_I() {
+    RomanCalculator romanCalculator = new RomanCalculator();
+    RomanNumeral roman1 = new RomanNumeral("I");
+    RomanNumeral roman2 = new RomanNumeral("I");
+    RomanNumeral result = romanCalculator.add(roman1, roman2);
+    assertEquals("II", result.getRomanNum());
+  }
+
+}
