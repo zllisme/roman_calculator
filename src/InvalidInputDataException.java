c585897ccd46828fb48a21cc6f94811e42e8db5d
jasmine.zhu <jasmine.zhu@oocl.com>
2020-01-12 19:02:09 +0800

    feat: roman number add given IIV and V throw InvalidInputDataException

--- src/InvalidInputDataException.java
+++ src/InvalidInputDataException.java
@@ -1,6 +1,6 @@
 public class InvalidInputDataException extends Exception{
 
-  public InvalidInputDataException(){
-    super("Roman number is more than one thousand");
+  public InvalidInputDataException(String message){
+    super(message);
   }
 }
--- src/RomanCalculator.java
+++ src/RomanCalculator.java
@@ -1,10 +1,17 @@
 class RomanCalculator {
   RomanNumeral add(RomanNumeral roman1, RomanNumeral roman2) throws InvalidInputDataException {
+    if(isRomanInvalid(roman1) || isRomanInvalid(roman2)) {
+      throw new InvalidInputDataException("Invalid roman numeral!");
+    }
     if (roman1.getIntValue() > 1000 || roman2.getIntValue() > 1000) {
-      throw new InvalidInputDataException();
+      throw new InvalidInputDataException("Roman number is more than one thousand");
     }
     int resultInt = roman1.getIntValue() + roman2.getIntValue();
     String romanNum = RomanNumeralConverter.convertIntToRoman(resultInt);
     return new RomanNumeral(romanNum, resultInt);
+  }
+
+  private boolean isRomanInvalid(RomanNumeral roman) {
+    return !roman.getRomanNum().equals(RomanNumeralConverter.convertIntToRoman(roman.getIntValue()));
   }
 }
