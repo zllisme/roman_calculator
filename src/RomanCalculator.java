b2d50c429aed262b835044f01292ee026dc81ef7
jasmine.zhu <jasmine.zhu@oocl.com>
2020-01-12 19:09:18 +0800

    refactor: extract method to validate input roman

--- src/RomanCalculator.java
+++ src/RomanCalculator.java
@@ -1,14 +1,18 @@
 class RomanCalculator {
   RomanNumeral add(RomanNumeral roman1, RomanNumeral roman2) throws InvalidInputDataException {
+    validateInputRoman(roman1, roman2);
+    int resultInt = roman1.getIntValue() + roman2.getIntValue();
+    String romanNum = RomanNumeralConverter.convertIntToRoman(resultInt);
+    return new RomanNumeral(romanNum, resultInt);
+  }
+
+  private void validateInputRoman(RomanNumeral roman1, RomanNumeral roman2) throws InvalidInputDataException {
     if(isRomanInvalid(roman1) || isRomanInvalid(roman2)) {
       throw new InvalidInputDataException("Invalid roman numeral!");
     }
     if (roman1.getIntValue() > 1000 || roman2.getIntValue() > 1000) {
       throw new InvalidInputDataException("Roman number is more than one thousand");
     }
-    int resultInt = roman1.getIntValue() + roman2.getIntValue();
-    String romanNum = RomanNumeralConverter.convertIntToRoman(resultInt);
-    return new RomanNumeral(romanNum, resultInt);
   }
 
   private boolean isRomanInvalid(RomanNumeral roman) {
