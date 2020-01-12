5a98f71a0fadabceaac53ce94555d22d425616dd
jasmine.zhu <jasmine.zhu@oocl.com>
2020-01-12 16:08:08 +0800

    feat: roman numeral I add II equal III

--- src/RomanCalculator.java
+++ src/RomanCalculator.java
@@ -1,5 +1,5 @@
 class RomanCalculator {
   RomanNumeral add(RomanNumeral roman1, RomanNumeral roman2) {
-    return new RomanNumeral("II");
+    return new RomanNumeral(roman1.getRomanNum() + roman2.getRomanNum());
   }
 }
