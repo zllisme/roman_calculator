0517d7f11fc49d058592786d52d6ed57c10acd6a
jasmine.zhu <jasmine.zhu@oocl.com>
2020-01-12 17:30:47 +0800

    feat: Roman numeral CM add DL equal MCDL

--- src/RomanNumeralConverter.java
+++ src/RomanNumeralConverter.java
@@ -8,12 +8,18 @@ public class RomanNumeralConverter {
       put('I', 1);
       put('V', 5);
       put('X', 10);
+      put('L', 50);
+      put('C', 100);
+      put('D', 500);
+      put('M', 1000);
     }
   };
 
   private static String[][] romanRule = new String[][]{
       {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
-      {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+      {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+      {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+      {"", "M"}
   };
 
   static int convertRomanToInt(String romanNumeral) {
@@ -26,8 +32,10 @@ public class RomanNumeralConverter {
     return resultInt;
   }
 
-  public static String convertIntToRoman(int intNum) {
+  static String convertIntToRoman(int intNum) {
     String romanNumeral = "";
+    romanNumeral += romanRule[3][intNum / 1000 % 10];
+    romanNumeral += romanRule[2][intNum / 100 % 10];
     romanNumeral += romanRule[1][intNum / 10 % 10];
     romanNumeral += romanRule[0][intNum % 10];
     return romanNumeral;
